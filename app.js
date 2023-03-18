9d6e07a255d34008eced86530f3ff93c7173b828
Michael <moreno.m2698@gmail.com>
2023-03-18 18:34:31 -0500

    add buttons

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hi keivan")
+
+function hiKeivan() {
+    console.log("hi keivan");
+  }
+function addHelloWorld() {
+    const myDiv = document.getElementById("myDiv");
+    const newParagraph = document.createElement("p");
+    const textNode = document.createTextNode("HelloWorld");
+    console.log(newParagraph);
+    console.log(myDiv);
+    newParagraph.appendChild(textNode);
+    myDiv.appendChild(newParagraph);
+}
